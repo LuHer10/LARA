35f5b4795f3df5509cd4f48893b4cd8bb3fa5c78
LuHer10 <lucas.herrera.eckert@gmail.com>
2025-06-18 13:00:40 +0200

    added base.h and base.cpp (Base class)

---
+++ robot/base.h
@@ -0,0 +1,10 @@
+#include "roboclaw.h"
+
+#define QPPR 1300 // Quadrature Pulses Per Revolution
+#define WHEEL_DIAMETER 0.06F // Wheel diameter in meters
+#define MAX_RPM 330 
+
+class Base
+{
+
+};
