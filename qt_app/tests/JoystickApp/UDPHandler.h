a2773105bb5011dd2124064c84bc6a705e81787b
LuHer10 <lucas.herrera.eckert@gmail.com>
2025-10-21 13:29:29 +0200

    changed server IP address

--- qt_app/tests/JoystickApp/UDPHandler.h
+++ qt_app/tests/JoystickApp/UDPHandler.h
@@ -22,7 +22,7 @@ private:
     QUdpSocket senderSocket;
     QUdpSocket receiverSocket;
 
-    QHostAddress serverAddress = QHostAddress("192.168.1.124"); // Replace with your server IP
+    QHostAddress serverAddress = QHostAddress("192.168.1.135"); // Replace with your server IP
     quint16 sendPort = 40000;
     quint16 receivePort = 40001;
 };
