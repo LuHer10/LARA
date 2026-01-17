672ee4595c719d9ac48929ef8025e970a7bb3849
LuHer10 <lucas.herrera.eckert@gmail.com>
2026-01-17 18:58:27 +0100

    updated arm test program

--- tests/udp_motor_network_arm.cpp
+++ tests/udp_motor_network_arm.cpp
@@ -6,6 +6,7 @@
 
 #include "../robot/base.h"
 #include "../robot/network.h"
+#include "../robot/arm.h"
 
 
 int main() {
@@ -17,6 +18,9 @@ int main() {
     float x_right = 0.0F;
     float y_right = 0.0F;
 
+    float q1, q2, q3;
+    float arm_x, arm_y, arm_th;
+
     int8_t grip;
 
     float pre_x_left, pre_y_left;
@@ -27,6 +31,11 @@ int main() {
     Odometry odo;
 
     Base base;
+
+    Arm arm(0.4f, 0.4f, 0.1f);
+    arm.get_qs(q1, q2, q3);
+    arm.get_pos(arm_x, arm_y, arm_th);
+    
     base.readEncoders();
     odo = base.getOdometry();
 
@@ -45,6 +54,9 @@ int main() {
 
         net.sendOdometry(odo.x, odo.y, odo.ang);
 
+        arm.get_qs(q1, q2, q3);
+        arm.DK(q1, q2, q3, arm_x, arm_y, arm_th);
+
         changed = (pre_x_left != x_left)
                 || (pre_y_left != y_left)
                 || (pre_x_right != x_right)
@@ -52,8 +64,18 @@ int main() {
 
         if(changed)
         {
-            base.setVelocity(x_left*0.5f, y_left*0.5f, -x_right);
-            base.sendSpeed();
+            if(arm_x > 0.7f || arm_x < 0.3f)
+            {
+                base.setVelocity(x_left*0.5f, y_left*0.5f, -x_right);
+                base.sendSpeed();
+                arm.moveIncr(0.0f, y_right*0.1f, 0.0f);
+            }
+            else
+            {
+                arm.moveIncr(y_left*0.1f, y_right*0.1f, 0.0f);
+            }
+
+            
         }
 
         //sleep(10);  // 10 Hz loop
