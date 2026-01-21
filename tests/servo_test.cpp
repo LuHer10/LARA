0d3c39edcccfdbbe238259d59ccaa11c911ccd6b
LuHer10 <lucas.herrera.eckert@gmail.com>
2026-01-21 12:23:07 +0100

    updates

--- tests/servo_test.cpp
+++ tests/servo_test.cpp
@@ -10,26 +10,10 @@ int main()
     servos.enableTorque(30);
     servos.enableTorque(40);
 
-    servos.writePos(11, 2);
-    servos.writePos(30, 2);
-    servos.writePos(40, 2);
-    sleep(1);
     servos.writePos(11, degToPos(90));
-    servos.writePos(30, degToPos(90));
-    servos.writePos(40, degToPos(90));
-    sleep(1);
-    servos.writePos(11, degToPos(180));
     servos.writePos(30, degToPos(180));
     servos.writePos(40, degToPos(180));
-    sleep(1);
-    servos.writePos(11, degToPos(90));
-    servos.writePos(30, degToPos(90));
-    servos.writePos(40, degToPos(90));
-    sleep(1);
-    servos.writePos(11, 2);
-    servos.writePos(30, 2);
-    servos.writePos(40, 2);
-    sleep(1);
+    sleep(3);
     servos.disableTorque(10);
     servos.disableTorque(20);
     servos.disableTorque(30);
