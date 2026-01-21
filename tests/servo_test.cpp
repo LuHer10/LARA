efc089972f1523661e7c520ac184dc8b011f7f99
LuHer10 <lucas.herrera.eckert@gmail.com>
2026-01-21 13:49:24 +0100

    test

--- tests/servo_test.cpp
+++ tests/servo_test.cpp
@@ -6,16 +6,20 @@ int main()
     Servo servos;
     servos.init();
 
+    //servos.extPosContMode(10);
+    //servos.extPosContMode(20);
+    //servos.extPosContMode(30);
+    //servos.extPosContMode(40);
+
     servos.enableTorque(11);
     servos.enableTorque(30);
     servos.enableTorque(40);
 
-    servos.writePos(11, degToPos(90));
-    servos.writePos(30, degToPos(180));
-    servos.writePos(40, degToPos(180));
+    servos.writePos(11, degToPos(0));
+    servos.writePos(30, degToPos(0));
+    servos.writePos(40, degToPos(0));
     sleep(3);
-    servos.disableTorque(10);
-    servos.disableTorque(20);
+    servos.disableTorque(11);
     servos.disableTorque(30);
     servos.disableTorque(40);
 }
