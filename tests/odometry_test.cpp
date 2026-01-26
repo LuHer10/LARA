2c76dc47e6ac5ab06cd6c917869d843190bf38b9
LuHer10 <lucas.herrera.eckert@gmail.com>
2026-01-26 19:47:45 +0100

    odometry test

---
+++ tests/odometry_test.cpp
@@ -0,0 +1,104 @@
+#include "../robot/base.h"
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+#define SERIAL_PORT "/dev/ttyAMA0"
+#define BAUD_RATE B38400
+
+#define INTERVAL_US 200000  // 200 ms in microseconds
+#define DIR_CHANGE_US 2000000
+
+long time_diff_us(struct timeval a, struct timeval b) {
+    return (a.tv_sec - b.tv_sec) * 1000000L +
+           (a.tv_usec - b.tv_usec);
+}
+
+int main() {
+    
+    //RoboClaw rcl(SERIAL_PORT, BAUD_RATE);
+    Base base;
+
+    Odometry odo;
+    
+    base.readEncoders();
+    odo = base.getOdometry();
+    std::cout << "Odometry start values:\nx: " << odo.x
+        << " , y: " << odo.y << " , ang: " << odo.ang << "\n";
+
+    float pre_x = odo.x; 
+    float pre_y = odo.y;
+
+
+    FILE *file = fopen("odometry.csv", "a");
+    if (!file) {
+        perror("fopen");
+        return 1;
+    }
+
+    fprintf(file, "Time; X; Y; Angle; VX; VY\n");
+    fflush(file);
+
+    struct timeval last, now, moveLast;
+    gettimeofday(&last, NULL);
+    gettimeofday(&moveLast, NULL);
+
+    int count = 0;
+
+    base.setVelocity(10.0f, 0.0f, 0.0f);
+    base.sendSpeed();
+
+    while (1) {
+        gettimeofday(&now, NULL);
+
+        if (time_diff_us(now, last) >= INTERVAL_US) {
+            odo = base.getOdometry();
+            fprintf(file, "%d; %f; %f; %f; %f; %f\n", now.tv_sec * 1000000L + now.tv_usec, 
+                odo.x, odo.y, odo.ang, odo.x-pre_x, odo.y-pre_y);
+            fflush(file);
+
+            last = now;  // reset reference time
+
+            pre_x = odo.x;
+            pre_y = odo.y;
+
+        }
+
+        if(time_diff_us(now, moveLast) >= DIR_CHANGE_US)
+        {
+            base.setVelocity(0.0f, 10.0f, 0.0f);
+            base.sendSpeed();
+        }
+
+        if(time_diff_us(now, moveLast) >= DIR_CHANGE_US*2)
+        {
+            base.setVelocity(-10.0f, 0.0f, 0.0f);
+            base.sendSpeed();
+        }
+
+        if(time_diff_us(now, moveLast) >= DIR_CHANGE_US*3)
+        {
+            base.setVelocity(0.0f, -10.0f, 0.0f);
+            base.sendSpeed();
+        }
+
+        if(time_diff_us(now, moveLast) >= DIR_CHANGE_US*4)
+        {
+            base.setVelocity(0.0f, 0.0f, 0.0f);
+            base.sendSpeed();
+            break;
+        }
+
+    }
+
+    fclose(file);
+    
+    base.readEncoders();
+    odo = base.getOdometry();
+    std::cout << "Odometry end values:\nx: " << odo.x
+        << " , y: " << odo.y << " , ang: " << odo.ang << "\n";
+    
+    return 0;
+}
+
